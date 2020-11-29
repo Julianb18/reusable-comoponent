af6d37010af268603ff63d749fb5652f6da6a59f
Julianb18 <julian141718@gmail.com>
2020-11-29 17:11:31 +0100

    app without styling

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,10 +2,10 @@ import React from "react";
 
 export const Article = ({ title, image, content }) => {
   return (
-    <div>
+    <div className="article-card">
       <h1>{title}</h1>
       <img src={image} alt="lol-image" />
-      <p>{content}</p>
+      <p className="content">{content}</p>
     </div>
   );
 };
